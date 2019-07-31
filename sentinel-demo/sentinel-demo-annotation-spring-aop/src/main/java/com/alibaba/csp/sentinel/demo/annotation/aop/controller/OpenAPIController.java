6b46b12f9a8c516809afdb42e3fd14533796659a
耕南 <gengnan.wy@alibaba-inc.com>
2019-07-31 11:44:22 +0800

    添加403 页面, 添加webservlet的拦截

---
+++ sentinel-demo/sentinel-demo-annotation-spring-aop/src/main/java/com/alibaba/csp/sentinel/demo/annotation/aop/controller/OpenAPIController.java
@@ -0,0 +1,10 @@
+package com.alibaba.csp.sentinel.demo.annotation.aop.controller;
+
+/**
+ * TODO MUST ADD COMMENT
+ *
+ * @author gengnan.wy
+ * @date 2019-07-31
+ */
+public class OpenAPIController {
+}
